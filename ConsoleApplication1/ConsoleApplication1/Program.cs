97c1fd2d5e9628b332611e7ff823215b8e6799fe
dilum1995 <dilumdesilva.edu@gmail.com>
2017-09-11 12:14:31 +0530

    Changes of QR generator

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt = DateTime.Now;
+            int xxxx = dt.Hour;
+            string xx = dt.ToString();
+            DateTime d = Convert.ToDateTime(xx);
+            int xxxx1 = d.Hour;
+        }
+    }
+}
